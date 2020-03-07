f7aa5393f3dbf37bd44a71f9f4012690c13fa4e4
rui-watanabe <wataru610806@gmail.com>
2020-03-07 01:19:39 +0000

    error-log function

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 const http = require('http');
 const server = http.createServer((req, res) =>
 {
+    console.info(`[${new Date()}] Requested by ${req.connection.remoteAddress}`);
     res.writeHead(200,
     {
         'Content-Type': 'text/plain; charset=utf8'
     });
     res.write(req.headers['user-agent']);
     res.end();
+})
+.on('error', (e) => 
+{
+    console.error(`[${new Date()}] Server Error`, e);
+})
+.on('clientError', (e) => 
+{
+    console.error(`[${new Date()}] Client Error`, e);
 });
 
 const port = 30000;
